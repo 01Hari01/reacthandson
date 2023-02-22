cbd8b243a5a66a3b7286fb727b2e5ee8a55c2351
01Hari01 <harikrishnathoka@gmail.com>
2023-02-22 14:58:59 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import VideoBackground from "./components/LandingPage";
+
+function App() {
+  return (
+    <div className="App">
+      <VideoBackground/>
+
+    </div>
+  );
+}
+
+export default App;
