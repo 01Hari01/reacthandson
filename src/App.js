9442a5c89ea1577e40ba9f9261664d37834e8406
01Hari01 <harikrishnathoka@gmail.com>
2023-02-26 17:48:04 +0000

    API call || About

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from "./components/Navbar/Navbar";
+import About from "./components/pages/About";
+import Home from "./components/pages/Home";
 
 function App() {
     return (
         <Router>
-            <div style={{display:"flex"}}>
-                <Navbar/>
+            <div style={{ display: 'flex' }}>
+                <Navbar />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/about" component={About} />
+                </Switch>
             </div>
         </Router>
     );
--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,19 +1,29 @@
 import React,{useState} from 'react';
-import {TextField} from "@mui/material";
+import {ListItem, TextField} from "@mui/material";
+import axios from 'axios';
 
 // API KEY:http://www.omdbapi.com/?i=tt3896198&apikey=4af4548c
 const About=()=>{
+    const [searchResults,setSearchResults]=useState([]);
     const handleSubmit=(e)=>{
         e.preventDefault();
+        const response= axios.get("http://www.omdbapi.com/?i=tt389");
+        setSearchResults(response.data)
     }
     const [searchQuery,setSearchQuery]= useState('');
     return(
         <div>
             <h1>About</h1>
             <form onSubmit={handleSubmit}>
-                <TextField variant="contained" label="Search for your movie" onChange={(e)=>setSearchQuery(e.target.value)} />
+                <TextField variant="contained" value={searchQuery} label="Search for your movie" onChange={(e)=>setSearchQuery(e.target.value)} />
+                {searchResults.map((items,index)=>(
+                    <ListItem key={items.id}>
+                        {items.title}
+                    </ListItem>
+                    ))}
 
             </form>
         </div>
     )
 }
+export default About;
